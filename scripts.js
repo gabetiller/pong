4e5287d55d70ee762657c2261e6d3419ba01bb1c
Gabe Tiller <gabectiller@gmail.com>
2018-02-25 12:13:41 -0500

    added paddle movement

--- scripts.js
+++ scripts.js
@@ -1,48 +1,120 @@
 
 window.onload = function() {
+  document.body.appendChild(canvas);
+  animate(step);
+};
 
-  var canvas = document.getElementById('table')
-  var ctx = canvas.getContext('2d');
+var canvas = document.createElement('canvas');
+var width = 400;
+var height = 600;
+canvas.width = width;
+canvas.height = height;
+var context = canvas.getContext('2d');
 
-  function table() {
-    ctx.fillRect(0, 0, 700, 500);
-    document.body.appendChild(canvas);
-  };
-  table();
 
-  function Paddle(x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-  };
+function Paddle(x, y, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.x_speed = 0;
+  this.y_speed = 0;
+}
 
-  Paddle.prototype.render = function() {
-    ctx.fillStyle = "#ffa500";
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  };
+Paddle.prototype.render = function() {
+  context.fillStyle = "#ffff00";
+  context.fillRect(this.x, this.y, this.width, this.height);
+};
 
-  function Ball(x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-  };
-
-  Ball.prototype.render = function() {
-    ctx.fillStyle = "#ffa500";
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  };
-
-  var computer = new Paddle(30, 205, 20, 90);
-  var player = new Paddle(650, 205, 20, 90);
-  var ball = new Ball(320, 240, 20, 20);
-
-  function render() {
-    computer.render();
-    player.render();
-    ball.render();
+Paddle.prototype.move = function(x, y) {
+  this.x += x;
+  this.y += y;
+  this.x_speed = x;
+  this.y_speed = y;
+  if(this.x < 0) {
+    this.x = 0;
+    this.x_speed = 0;
+  } else if (this.x + this.width > 400) {
+    this.x = 400 - this.width;
+    this.x_speed = 0;
   }
-  render();
+}
 
+function Ball(x, y) {
+  this.x = x;
+  this.y = y;
+  this.x_speed = 0;
+  this.y_speed = 3;
+  this.radius = 13;
+}
+
+Ball.prototype.render = function() {
+  context.beginPath();
+  context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+  context.fillStyle = "#ffff00";
+  context.fill();
+};
+
+function Player() {
+  this.paddle = new Paddle(175, 580, 90, 10);
+}
+
+function Computer() {
+  this.paddle = new Paddle(175, 10, 90, 10);
+}
+
+Player.prototype.render = function() {
+  this.paddle.render();
+};
+
+Player.prototype.update = function() {
+  for(var key in keysDown) {
+    var value = Number(key);
+    if(value == 37) { // left arrow
+      this.paddle.move(-4, 0);
+    } else if (value == 39) { // right arrow
+      this.paddle.move(4, 0);
+    } else {
+      this.paddle.move(0, 0);
+    }
+  }
+};
+
+Computer.prototype.render = function() {
+  this.paddle.render();
+};
+
+var player = new Player();
+var computer = new Computer();
+var ball = new Ball(200, 300)
+
+var keysDown = {};
+
+window.addEventListener("keydown", function(event) {
+  keysDown[event.keyCode] = true;
+});
+
+window.addEventListener("keyup", function(event) {
+  delete keysDown[event.keyCode];
+});
+
+var animate = window.requestAnimationFrame ||
+function(callback) { window.setTimeout(callback, 1000/60) };
+
+function render() {
+  context.fillStyle = "#000000";
+  context.fillRect(0, 0, width, height);
+  player.render();
+  computer.render();
+  ball.render();
+};
+
+function update() {
+  player.update();
+};
+
+function step() {
+  update();
+  render();
+  animate(step);
 };
