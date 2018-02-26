75d4d5986e51d18400a7dfaaabd5aa8d9289378a
Gabe Tiller <gabectiller@gmail.com>
2018-02-26 10:05:31 -0500

    animated ball

--- scripts.js
+++ scripts.js
@@ -40,6 +40,27 @@ Paddle.prototype.move = function(x, y) {
   }
 }
 
+function Player() {
+  this.paddle = new Paddle(160, 580, 90, 10);
+};
+
+Player.prototype.render = function() {
+  this.paddle.render();
+};
+
+Player.prototype.update = function() {
+  for(var key in keysDown) {
+    var value = Number(key);
+    if(value == 37) {
+      this.paddle.move(-6, 0);
+    } else if (value == 39) {
+      this.paddle.move(6, 0);
+    } else {
+      this.paddle.move(0, 0);
+    }
+  }
+};
+
 function Ball(x, y) {
   this.x = x;
   this.y = y;
@@ -55,30 +76,42 @@ Ball.prototype.render = function() {
   context.fill();
 };
 
-function Player() {
-  this.paddle = new Paddle(175, 580, 90, 10);
-}
+Ball.prototype.update = function(paddle1, paddle2) {
+  this.x += this.x_speed;
+  this.y += this.y_speed;
+  var top_x = this.x - 5;
+  var top_y = this.y - 5;
+  var bottom_x = this.x + 5;
+  var bottom_y = this.y + 5;
 
-function Computer() {
-  this.paddle = new Paddle(175, 10, 90, 10);
-}
+  if(this.x - 5 < 0) {
+    this.x = 5;
+    this.x_speed = -this.x_speed;
+  } else if(this.x + 5 > 400) {
+    this.x = 395;
+    this.x_speed = -this.x_speed;
+  }
 
-Player.prototype.render = function() {
-  this.paddle.render();
-};
+  if(this.y < 0 || this.y > 600) {
+    this.x_speed = 0;
+    this.y_speed = 3;
+    this.x = 200;
+    this.y = 300;
+  }
 
-Player.prototype.update = function() {
-  for(var key in keysDown) {
-    var value = Number(key);
-    if(value == 37) { // left arrow
-      this.paddle.move(-4, 0);
-    } else if (value == 39) { // right arrow
-      this.paddle.move(4, 0);
-    } else {
-      this.paddle.move(0, 0);
+  if(top_y > 300) {
+    if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
+      this.y_speed = -3;
+      this.x_speed += (paddle1.x_speed / 2);
+      this.y += this.y_speed;
     }
   }
 };
+
+function Computer() {
+  this.paddle = new Paddle(160, 10, 90, 10);
+}
+
 
 Computer.prototype.render = function() {
   this.paddle.render();
@@ -110,6 +143,7 @@ function render() {
 };
 
 function update() {
+  ball.update(player.paddle, computer.paddle)
   player.update();
 };
 
